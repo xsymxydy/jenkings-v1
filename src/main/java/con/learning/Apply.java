2ac30adb13a5918a33357a1b7d102e8c7e040878
xsymxydy <xsymx_ydy@126.com>
2020-05-07 15:25:25 +0800

    jenkins-v1

---
+++ src/main/java/con/learning/Apply.java
@@ -0,0 +1,18 @@
+package con.learning;
+
+/**
+ * This is Description
+ *
+ * @author dongnanrui
+ * @date 2020/05/07
+ */
+public class Apply {
+
+    private String appName = "Goold apply";
+
+    public String getAppName() {
+        return appName;
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page import="con.learning.Apply" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+<h1><%=new Apply().getAppName()%></h1>
+</body>
+</html>
